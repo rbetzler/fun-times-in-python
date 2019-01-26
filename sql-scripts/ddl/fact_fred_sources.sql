ca76e4a8efba0f95e816059efdfa5202789add7b
rbetzler <rbetzler@94.gmail.com>
2019-01-26 18:52:29 -0500

    fixed a few tabs in sql files

--- sql-scripts/ddl/fact_fred_sources.sql
+++ sql-scripts/ddl/fact_fred_sources.sql
@@ -4,13 +4,13 @@
 
 CREATE TABLE public.fact_fred_sources
 (
-  source_id numeric,
-  link text,
-  name text,
-  realtime_end date,
-  realtime_start date,
-  created_at timestamp without time zone,
-  updated_at timestamp without time zone
+  source_id       numeric,
+  link            text,
+  name            text,
+  realtime_end    date,
+  realtime_start  date,
+  created_at      timestamp without time zone,
+  updated_at      timestamp without time zone
 )
 WITH (
   OIDS=FALSE
