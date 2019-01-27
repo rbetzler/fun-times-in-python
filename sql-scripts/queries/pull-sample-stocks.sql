172981d8345d8640f9c88d2d756f7b1a9f1e2a2c
rbetzler <rbetzler@94.gmail.com>
2019-01-27 17:36:43 -0500

    added prediction to time series

--- sql-scripts/queries/pull-sample-stocks.sql
+++ sql-scripts/queries/pull-sample-stocks.sql
@@ -1,3 +1,17 @@
-select *
+select
+	ticker,
+	date_time,
+	open,
+	high,
+	low,
+	close,
+	adj_close,
+	volume,
+	dividend,
+	split_numerator,
+	split_denominator,
+	unix_timestamp
 from fact_yahoo_stocks
 where ticker = 'AAPL'
+order by date_time
+limit 8000
