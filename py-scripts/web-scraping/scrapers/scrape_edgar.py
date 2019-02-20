5c5946b424b5def7ecea3aad627a989e783e24c0
rbetzler <rbetzler@94.gmail.com>
2019-02-20 00:03:31 -0500

    added requests for edgar scraper, need to figure out if ingest df or list

--- py-scripts/web-scraping/scrapers/scrape_edgar.py
+++ py-scripts/web-scraping/scrapers/scrape_edgar.py
@@ -6,47 +6,48 @@ Created on Sat Feb 16 16:56:11 2019
 @author: nautilus
 """
 
+import time
 import requests
 import pandas as pd
 
-def ConvertFileHtmPathToTxt(df, company_id, file_path):
-    
-    df['url'] = ('http://www.sec.gov/Archives/edgar/data/' 
-      + df[company_id] 
+def ConvertHtmPathToTxt(df, company_id, file_path):
+
+    df['url'] = ('http://www.sec.gov/Archives/edgar/data/'
+      + df[company_id]
       + '/'
-      + df[file_path].str.split('/', expand = True)[7].str.rsplit('-', expand = True, n = 1)[0].str.replace('-', '') 
+      + df[file_path].str.split('/', expand = True)[7].str.rsplit('-', expand = True, n = 1)[0].str.replace('-', '')
       + '/'
       + df[file_path].str.split('/', expand = True)[7].str.rsplit('-', expand = True, n = 1)[0]
       + '.txt')
-    
+
     return df
 
 def ExtractEdgarIndex(year, quarter, date):
-    
+
     #Get crawler index
-    url = ('https://www.sec.gov/Archives/edgar/daily-index/' 
+    url = ('https://www.sec.gov/Archives/edgar/daily-index/'
            + year + '/' + quarter + '/crawler.' + date + '.idx')
     edgar_index = requests.get(url).text
-    
+
     #Covert to list
     edgar_index = edgar_index.split('\n')
-    
+
     #Drop header
     for row in range(0, len(edgar_index)):
         characters = set(edgar_index[row])
         if characters == {'-'}:
             edgar_index = edgar_index[row+1:]
             break
-    
+
     #Convert to pandas
     df_edgar = pd.Series(data = edgar_index)
-    
+
     #Partition positions
     part_one = 62
     part_two = 74
     part_three = 86
     part_four = 98
-    
+
     #Split to columns
     df = pd.DataFrame({
             'company' : df_edgar.str.slice(stop = part_one).str.strip(),
@@ -54,12 +55,34 @@ def ExtractEdgarIndex(year, quarter, date):
             'company_id' : df_edgar.str.slice(start = part_two, stop = part_three).str.strip(),
             'date' : df_edgar.str.slice(start = part_three, stop = part_four).str.strip(),
             'file_path' : df_edgar.str.slice(start = part_four).str.strip()})
-    
-    df = ConvertFileHtmPathToTxt(df, 'company_id', 'file_path')
-    
+
+    df = ConvertHtmPathToTxt(df, 'company_id', 'file_path')
+
     return df
 
+def RetreiveEdgar(urls, time_delay = 4):
+    
+    docs = []
+    for url in urls:
+        docs.append(requests.get(url).text)
+        print('Received txt... Sleeping')
+        time.sleep(time_delay)
+        print('Ok back at it')
+        
+    return docs
 
+def RetreiveEdgarDf(df, urls, time_delay = 4):
+    
+    df.loc[:, 'doc'] = pd.Series('')
+    
+    for idx, row in df[urls].iteritems():
+        df.loc[idx, 'doc'] = requests.get(row).text
+        print('Received txt... Sleeping')
+        time.sleep(time_delay)
+        print('Ok back at it')
+        
+    return df
+    
 ##Example
 #year = '2019'
 #quarter = 'QTR1'
