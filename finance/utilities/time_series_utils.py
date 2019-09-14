80cd585730f95e058d06ce95831c3fdf8db3fcae
Rick Betzler <rick.betzler@petalcard.com>
2019-09-14 00:52:27 -0400

    wip on arima base class

--- finance/utilities/time_series_utils.py
+++ finance/utilities/time_series_utils.py
@@ -1,22 +1,88 @@
 import math
 import pandas as pd
 import statsmodels.tsa.stattools as sm_tools
-
+import statsmodels.tsa.arima_model as stats_arima
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 import matplotlib.pyplot as plt
+
+from finance.utilities import utils
 
 
 class ARIMA:
-    def __init__(self,
-                 series=pd.DataFrame,
-                 alpha=.05,
-                 limit=1000):
+    def __init__(self, series=pd.DataFrame, datetimes=None, forecast_date=None,
+                 alpha=.05, limit=1000, frequency='B', p=1, d=1, q=1):
         self.series = series
+        self.datetimes = datetimes
+        self.forecast_date = pd.to_datetime(forecast_date)
         self.alpha = alpha
         self.limit = limit
+        self.frequency = frequency
+        self.p = p
+        self.d = d
+        self.q = q
+
+    @property
+    def model_df(self):
+        df = pd.DataFrame(data=self.series)
+        df.columns = ['vals']
+
+        df.index = pd.DatetimeIndex(self.datetimes.dt.date)
+        df.index.names = ['date']
+        df.sort_index()
+
+        df = df.resample(self.frequency).bfill()
+        df.index.freq = self.frequency
+        return df
+
+    @staticmethod
+    def model_forecast_helper(df=None, forecast_date=None, p=0, d=0, q=0, frequency='B') -> tuple:
+        training_set = df[df.index < forecast_date]['vals']
+        dates = df[df.index < forecast_date].index
+        params = p, d, q
+        model = stats_arima.ARIMA(training_set, params, dates=dates, freq=frequency).fit()
+        forecast = model.forecast()[0][0]
+        aic = model.aic
+        bic = model.bic
+        return forecast, aic, bic
+
+    @property
+    def model_forecast(self):
+        forecast = self.model_forecast_helper(df=self.model_df,
+                                              forecast_date=self.forecast_date,
+                                              p=self.p,
+                                              d=self.d,
+                                              q=self.q,
+                                              frequency=self.frequency)
+        return forecast
+
+    @property
+    def optimal_params(self, criterion='mse') -> pd.DataFrame:
+        performances = []
+        df = self.model_df
+        frequency = self.frequency
+        for forecast_date in utils.iter_date_range(self.forecast_date, df.index.max()):
+            for p in range(0, self.p):
+                for d in range(0, self.d):
+                    for q in range(0, self.q):
+                        forecast, aic, bic = self.model_forecast_helper(df=df, forecast_date=forecast_date,
+                                                                        p=p, d=d, q=q, frequency=frequency)
+                        actual = df[df.index == forecast_date]
+                        performance = {'forecast_date': forecast_date,
+                                       'p': p,
+                                       'd': d,
+                                       'q': q,
+                                       'aic': aic,
+                                       'bic': bic,
+                                       'mse': mean_squared_error(y_true=actual, y_pred=[forecast]),
+                                       'mae': mean_absolute_error(y_true=actual, y_pred=[forecast])}
+                        performances.append(performance)
+        perf_df = pd.DataFrame(performances).groupby(['p', 'd', 'q']).mean()
+        optimal_param = perf_df[perf_df[criterion] == perf_df[criterion].min()]
+        return optimal_param
 
     @property
     def acf_vals(self):
-        acf = sm_tools.acf(self.series.head(self.limit), alpha=self.alpha)
+        acf = sm_tools.acf(self.series.head(self.limit), alpha=self.alpha, fft=False)
         return acf
 
     def acf_plot(self):
@@ -37,7 +103,6 @@ class ARIMA:
 
     def pacf_plot(self):
         plt.figure()
-        plt.figure()
         plt.title('PACF of ' + self.series.name)
         plt.xlabel('Lags')
         plt.plot(self.pacf_vals[0], linestyle='-', color='b')
@@ -47,6 +112,30 @@ class ARIMA:
 
 
 if __name__ == '__main__':
-    x = pd.DataFrame([1,2,3,4,5])
-    var = ARIMA(series=x).pacf_plot()
+    query = """
+        select
+            e.symbol
+            , e.market_datetime
+            , e.open
+            , e.high
+            , e.low
+            , e.close
+            , e.volume
+            , f.high_52
+            , f.low_52
+            , f.dividend_amount
+            , f.pe_ratio
+            , f.quick_ratio
+            , f.current_ratio
+        from td.equities as e
+        left join td.fundamentals as f
+            on f.symbol = e.symbol
+        where e.symbol = 'BA'
+        order by e.market_datetime
+        """
+    df = utils.query_db(query=query)
+    var = ARIMA(series=df['open'],
+                datetimes=df['market_datetime'],
+                p=3,
+                forecast_date='2019-09-02').optimal_params
     print(var)
--- finance/utilities/utils.py
+++ finance/utilities/utils.py
@@ -1,6 +1,7 @@
 import os
 import psycopg2
 import pandas as pd
+import datetime
 
 
 # DW_STOCKS = 'postgresql://postgres:password@172.18.0.2:5432/dw_stocks'
@@ -34,3 +35,13 @@ def retrieve_secret(var, pwd=''):
 def create_directory(directory):
     if not os.path.exists(directory):
         os.mkdir(directory)
+
+
+def iter_date_range(start_date, end_date):
+    if not isinstance(start_date, datetime.datetime):
+        start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d')
+    if not isinstance(end_date, datetime.datetime):
+        end_date = datetime.datetime.strptime(end_date, '%Y-%m-%d')
+    days_diff = (end_date - start_date).days
+    for n in range(days_diff):
+        yield start_date + datetime.timedelta(n)
