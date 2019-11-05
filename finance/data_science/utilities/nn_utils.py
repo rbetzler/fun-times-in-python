641ca4f44a6b6c31e1ef84e0b1fa38be2dc1f0fe
rbetzler <rbetzler@94.gmail.com>
2019-11-05 00:05:18 -0500

    working on nns

--- finance/data_science/utilities/nn_utils.py
+++ finance/data_science/utilities/nn_utils.py
@@ -1,92 +1,63 @@
 import pandas as pd
 import torch
-from torch import nn, functional, optim, autograd
-
-from finance.utilities import utils
-from finance.data_science.utilities import cluster_utils
+from torch import nn, optim
 
 
-class TorchNN:
+class TorchNN(nn.Module):
     def __init__(self,
                  train_x=pd.DataFrame,
                  train_y=pd.DataFrame,
-                 test_x=pd.DataFrame,
-                 test_y=pd.DataFrame,
+                 hidden_size=8,
+                 output_size=1,
                  learning_rate=0,
-                 momentum=.9,
-                 hidden_size=None,
-                 out_features=1):
-        self.train_x = torch.tensor(train_x.values).float().view(1, -1)
+                 momentum=.9):
+
+        self.train_x = torch.tensor(train_x.values).float().view(1, -1).detach().requires_grad_(True)
         self.train_y = torch.tensor(train_y.values).float()
-        # self.test_x = torch.tensor(test_x)
-        # self.test_y = torch.tensor(test_y)
+
         self.size = train_x.shape[0] * train_x.shape[1]
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+
         self.learning_rate = learning_rate
         self.momentum = momentum
-        self.linear_one = nn.Linear(in_features=self.size, out_features=hidden_size[0]).float()
-        self.linear_two = nn.Linear(in_features=hidden_size[0], out_features=out_features).float()
 
-    def forward(self):
-        output = self.linear_one(self.train_x.view(self.train_x.size(0), -1))
-        output = functional.F.relu(output)
-        output = self.linear_two(output)
-        return output
+    @property
+    def layers(self):
+        layers = {
+            'linear_one': nn.Linear(in_features=self.size, out_features=self.hidden_size).float(),
+            'linear_two': nn.Linear(in_features=self.hidden_size, out_features=self.output_size).float()
+        }
+        return layers
 
-    def optimizer(self, model):
-        optimizer = optim.SGD(nn.Module.parameters(model), lr=self.learning_rate, momentum=self.momentum)
-        return optimizer
+    def forward(self, data):
+        for layer in self.layers:
+            data = self.layers.get(layer)(data)
+        return data
 
-    def loss_criterion(self):
-        return nn.NLLLoss()
+    def optimizer(self):
+        return optim.SGD([self.train_x], lr=self.learning_rate, momentum=self.momentum)
 
-    def train_network(self, model):
-        for idx, row in self.train_x.iterrows():
-            data = autograd.Variable(row)
-            target = autograd.Variable(self.train_y[idx])
+    @property
+    def criterion(self):
+        return nn.MSELoss()
 
-            self.optimizer.zero_grad()
-            net_out = model(data)
-            loss = self.loss_criterion
-            loss(net_out, target)
-            loss.backward()
+    @property
+    def n_epochs(self):
+        return 2
 
-            self.optimizer.step()
+    def train_network(self):
+        running_loss = 0
+        optimizer = self.optimizer()
+        criterion = self.criterion
 
+        for epoch in range(self.n_epochs):
+            for data, target in zip(self.train_x, self.train_y):
+                optimizer.zero_grad()
 
-if __name__ == '__main__':
-    query = """
-        select
-            e.symbol
-            , e.market_datetime
-            , e.open
-            , e.high
-            , e.low
-            , e.close
-            , e.volume
-        from td.equities as e
-        where 
-        --left(e.symbol, 1) = 'A'
-        e.symbol = 'AA'
-        order by e.market_datetime
-        limit 100
-        """
-    df = utils.query_db(query=query)
-    for col in ['open']:
-        df = cluster_utils.normalize(df=df, column=col, subset='symbol')
-    df['market_datetime'] = df['market_datetime'].astype(int)
+                outputs = self.forward(data)
+                loss = criterion(outputs, target)
+                loss.backward()
+                optimizer.step()
 
-    train = df.copy()
-    train = train.drop(columns=['symbol'])
-    train['market_datetime'] = train['market_datetime'].astype(int)
-
-    train_target = train['open'].shift(-1)
-
-    split_size = 80000
-    train_x = train.iloc[1:split_size]
-    train_y = train_target.iloc[1:split_size]
-
-    test_x = train.iloc[split_size + 1:-1]
-    test_y = train_target.iloc[split_size + 1:-1]
-
-    model = TorchNN(train_x=train_x, train_y=train_y, hidden_size=(1,)).forward()
-    print(model)
+                running_loss += loss.item()
