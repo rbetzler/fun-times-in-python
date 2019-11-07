445c40fa4f3bc64453dcd4ca0d1a1bde3852f762
rbetzler <rbetzler@94.gmail.com>
2019-11-07 00:53:48 -0500

    tweaks to ds stuffs

--- finance/data_science/utilities/nn_utils.py
+++ finance/data_science/utilities/nn_utils.py
@@ -1,10 +1,14 @@
 import pandas as pd
 import torch
-from torch import nn, optim
+from torch import nn, functional, optim, autograd
+
+from finance.utilities import utils
+from finance.data_science.utilities import cluster_utils
 
 
 class TorchNN(nn.Module):
     def __init__(self,
+                 device='cuda:0',
                  train_x=pd.DataFrame,
                  train_y=pd.DataFrame,
                  hidden_size=8,
@@ -12,8 +16,12 @@ class TorchNN(nn.Module):
                  learning_rate=0,
                  momentum=.9):
 
-        self.train_x = torch.tensor(train_x.values).float().view(1, -1).detach().requires_grad_(True)
-        self.train_y = torch.tensor(train_y.values).float()
+        super().__init__()
+
+        self.device = device
+
+        self.train_x = torch.tensor(train_x.values).to(self.device).float().view(1, -1).detach().requires_grad_(True)
+        self.train_y = torch.tensor(train_y.values).to(self.device).float()
 
         self.size = train_x.shape[0] * train_x.shape[1]
         self.hidden_size = hidden_size
@@ -25,11 +33,12 @@ class TorchNN(nn.Module):
     @property
     def layers(self):
         layers = {
-            'linear_one': nn.Linear(in_features=self.size, out_features=self.hidden_size).float(),
-            'linear_two': nn.Linear(in_features=self.hidden_size, out_features=self.output_size).float()
+            'linear_one': nn.Linear(in_features=self.size, out_features=self.hidden_size).float().to(self.device),
+            'linear_two': nn.Linear(in_features=self.hidden_size, out_features=self.output_size).float().to(self.device)
         }
         return layers
 
+    # make this forward pass work for predictions
     def forward(self, data):
         for layer in self.layers:
             data = self.layers.get(layer)(data)
@@ -40,7 +49,7 @@ class TorchNN(nn.Module):
 
     @property
     def criterion(self):
-        return nn.MSELoss()
+        return nn.MSELoss().to(self.device)
 
     @property
     def n_epochs(self):
@@ -55,9 +64,11 @@ class TorchNN(nn.Module):
             for data, target in zip(self.train_x, self.train_y):
                 optimizer.zero_grad()
 
-                outputs = self.forward(data)
-                loss = criterion(outputs, target)
+                outputs = self.forward(data).to(self.device)
+                loss = criterion(outputs, target).to(self.device)
                 loss.backward()
                 optimizer.step()
 
                 running_loss += loss.item()
+
+        return running_loss
