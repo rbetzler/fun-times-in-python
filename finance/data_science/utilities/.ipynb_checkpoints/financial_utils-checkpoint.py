a8903db9e6a6c7d03c4cf3b1770118385bcc245a
rbetzler <rbetzler@94.gmail.com>
2020-01-20 23:23:52 -0500

    redoing portfolio optimization

--- finance/data_science/utilities/.ipynb_checkpoints/financial_utils-checkpoint.py
+++ finance/data_science/utilities/.ipynb_checkpoints/financial_utils-checkpoint.py
@@ -152,6 +152,76 @@ class BlackScholes:
         return diff
 
 
-def kelly_criterion(predicted_win, predicted_loss, p_win, p_loss):
-    bet_size = (predicted_win * p_win - predicted_loss * p_loss) / predicted_win
+def annualized_return(start_price, end_price, n_days):
+    annual_return = (1+((end_price-start_price)/start_price))**(365/n_days)-1
+    return annual_return
+
+
+def kelly_criterion(predicted_win, predicted_loss, p_win):
+    bet_size = (predicted_win * p_win - predicted_loss * (1 - p_win)) / predicted_win
     return bet_size
+
+
+def get_estimated_loss(df, 
+                       profits: str='profits',
+                       window_size: int=100,
+                       loss: str='median'
+                      ):
+    df['is_profitable'] = False
+    df.loc[df[profits] > 0, 'is_profitable'] = True
+
+    df['profit_rate'] = (df['is_profitable'].rolling(window_size).sum()
+                          /df['is_profitable'].rolling(window_size).count())
+    
+    if loss == 'mean':
+        df['estimated_loss'] = abs(df[profits]).rolling(window_size).mean()
+    elif loss == 'median':
+        df['estimated_loss'] = abs(df[profits]).rolling(window_size).median()
+
+    return df.drop(['is_profitable'], axis=1)
+
+
+def greedy_kelly(df,
+                 level: str='market_datetime',
+                 kelly: str='kelly',
+                 price: str='open',
+                 profits: str='profits',
+                 budget: float=1000,
+                ):
+    df['position'] = 0
+    df['n_shares'] = 0
+
+    positions_dfs = []
+    for idx, temp in df.groupby(level):
+        stocks = temp[temp['kelly'] > 0]
+        positions = stocks.loc[stocks.sort_values(by=kelly, ascending=False)[kelly].cumsum() < 1].copy()
+        positions['n_shares'] = ((positions[kelly] * budget)/positions[price]).astype(int)
+        positions['position'] = positions['n_shares'] * positions[price]
+        budget += sum(positions['n_shares'] * positions[profits])
+        positions_dfs.append(positions)
+
+    return pd.concat(positions_dfs)
+
+
+def n_largest_kelly(df,
+                    n_stocks: int=3,
+                    level: str='market_datetime',
+                    kelly: str='kelly',
+                    price: str='open',
+                    profits: str='profits',
+                    budget: float=1000,
+                   ):
+    df['position'] = 0
+    df['n_shares'] = 0
+
+    positions_dfs = []
+    for idx, temp in df.groupby(level):
+        stocks = temp[temp[kelly] > 0]
+        positions = stocks.nlargest(n_stocks, kelly)
+        df[kelly] = df[kelly] * (1 / df[kelly].sum())
+        positions['n_shares'] = ((positions[kelly] * budget)/positions[price]).astype(int)
+        positions['position'] = positions['n_shares'] * positions[price]
+        budget += sum(positions['n_shares'] * positions[profits])
+        positions_dfs.append(positions)
+
+    return pd.concat(positions_dfs)
