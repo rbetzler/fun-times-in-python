d51c35469207cbbe8df0afb1a52e071eb6e8c440
Rick Betzler <rick.betzler@petalcard.com>
2020-05-13 20:11:32 -0400

    adding logs and reformatting table creator
    adding abc decorators

--- finance/ingestion/table_creator.py
+++ finance/ingestion/table_creator.py
@@ -1,4 +1,5 @@
 import abc
+import datetime
 import psycopg2
 from finance.utilities import utils
 
@@ -10,18 +11,22 @@ class TableCreator(abc.ABC):
         return utils.DW_STOCKS
 
     @property
+    @abc.abstractmethod
     def schema_name(self) -> str:
         pass
 
     @property
+    @abc.abstractmethod
     def table_name(self) -> str:
         pass
 
     @property
+    @abc.abstractmethod
     def table_ddl(self) -> str:
         pass
 
     @property
+    @abc.abstractmethod
     def sql_script(self) -> str:
         pass
 
@@ -29,19 +34,28 @@ class TableCreator(abc.ABC):
         conn = psycopg2.connect(self.db_connection)
         cursor = conn.cursor()
 
-        schema_check = "SELECT EXISTS (SELECT 1 FROM information_schema.schemata " \
-                       + "WHERE schema_name = '" + self.schema_name + "')"
+        print(f'Checking if schema exists: {datetime.datetime.utcnow()}')
+        schema_check = f"""
+            SELECT EXISTS (
+                SELECT 1
+                FROM information_schema.schemata
+                WHERE schema_name = '{self.schema_name}'
+            )
+            """
         cursor.execute(schema_check)
         schema_exists = cursor.fetchone()[0]
         if not schema_exists:
-            cursor.execute('CREATE SCHEMA ' + self.schema_name + ' ; ')
+            print(f'Schema does not exist; creating now: {datetime.datetime.utcnow()}')
+            cursor.execute(f'CREATE SCHEMA {self.schema_name};')
             conn.commit()
 
         if self.table_ddl:
+            print(f'Running table ddl just in case: {datetime.datetime.utcnow()}')
             cursor.execute(self.table_ddl)
             conn.commit()
 
         if self.sql_script:
+            print(f'Running sql script: {datetime.datetime.utcnow()}')
             cursor.execute(self.sql_script)
             conn.commit()
 
