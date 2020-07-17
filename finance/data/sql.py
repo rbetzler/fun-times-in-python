0b9bbc64b5fd46f491f702a61f16f124392f5b19
rbetzler <rbetzler@94.gmail.com>
2020-07-16 22:08:52 -0400

    adding db maintenance script

--- finance/data/sql.py
+++ finance/data/sql.py
@@ -30,44 +30,56 @@ class SQLRunner(abc.ABC):
     def sql_script(self) -> str:
         pass
 
+    @property
+    def is_maintenance(self) -> bool:
+        """Maintenance scripts (like vacuums) require toggling autocommit on and off"""
+        return False
+
     def execute(self):
         conn = psycopg2.connect(self.db_connection)
         cursor = conn.cursor()
 
-        schema_query = f'''
-            SELECT EXISTS (
-                SELECT 1
-                FROM information_schema.schemata
-                WHERE schema_name = '{self.schema_name}'
-            )
-            '''
-        print(f'Checking if schema exists: {datetime.datetime.utcnow()}')
-        cursor.execute(schema_query)
-        schema_exists = cursor.fetchone()[0]
-        if not schema_exists:
-            print(f'Schema does not exist; creating now: {datetime.datetime.utcnow()}')
-            cursor.execute(f'CREATE SCHEMA {self.schema_name};')
-            conn.commit()
+        if self.schema_name:
+            schema_query = f'''
+                SELECT EXISTS (
+                    SELECT 1
+                    FROM information_schema.schemata
+                    WHERE schema_name = '{self.schema_name}'
+                )
+                '''
+            print(f'Checking if schema exists: {datetime.datetime.utcnow()}')
+            cursor.execute(schema_query)
+            schema_exists = cursor.fetchone()[0]
+            if not schema_exists:
+                print(f'Schema does not exist; creating now: {datetime.datetime.utcnow()}')
+                cursor.execute(f'CREATE SCHEMA {self.schema_name};')
+                conn.commit()
 
-        table_query = f'''
-            SELECT EXISTS (
-                SELECT 1
-                FROM information_schema.tables
-                WHERE table_name = '{self.table_name}'
-            )
-            '''
-        print(f'Checking if table exists: {datetime.datetime.utcnow()}')
-        cursor.execute(table_query)
-        table_exists = cursor.fetchone()[0]
-        if not table_exists:
-            print(f'Table does not exist; creating now: {datetime.datetime.utcnow()}')
-            cursor.execute(self.table_ddl)
-            conn.commit()
+        if self.table_name:
+            table_query = f'''
+                SELECT EXISTS (
+                    SELECT 1
+                    FROM information_schema.tables
+                    WHERE table_name = '{self.table_name}'
+                )
+                '''
+            print(f'Checking if table exists: {datetime.datetime.utcnow()}')
+            cursor.execute(table_query)
+            table_exists = cursor.fetchone()[0]
+            if not table_exists:
+                print(f'Table does not exist; creating now: {datetime.datetime.utcnow()}')
+                cursor.execute(self.table_ddl)
+                conn.commit()
 
         if self.sql_script:
             print(f'Running sql script: {datetime.datetime.utcnow()}')
-            cursor.execute(self.sql_script)
-            conn.commit()
+            if self.is_maintenance:
+                conn.autocommit = True
+                cursor.execute(self.sql_script)
+                conn.autocommit = False
+            else:
+                cursor.execute(self.sql_script)
+                conn.commit()
 
         conn.close()
         cursor.close()
