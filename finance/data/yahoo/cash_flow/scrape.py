c15ef97453e0f693bf8b359f27f80895f2ade20e
rbetzler <rbetzler@94.gmail.com>
2020-07-19 16:03:24 -0400

    refactored yahoo cash flows

--- finance/data/yahoo/cash_flow/scrape.py
+++ finance/data/yahoo/cash_flow/scrape.py
@@ -1,53 +1,63 @@
 import re
-import time
 import pandas as pd
 from finance.data import scraper
 
 
-class CashFlowsScraper(scraper.Caller):
+class CashFlow(scraper.Caller):
     @property
     def job_name(self) -> str:
-        return 'yahoo_cash_flows'
+        return 'yahoo_cash_flow'
 
     @property
-    def calls_query(self) -> str:
-        query = f"select distinct ticker from nasdaq.listed_stocks "\
-                f"where ticker !~ '[\^.~]' and character_length(ticker) between 1 and 4 limit 10;"
+    def export_folder(self) -> str:
+        folder = f'audit/yahoo/cash_flow/{self.folder_datetime}/'
+        return folder
+
+    @property
+    def export_file_name(self) -> str:
+        return 'yahoo_cash_flow_'
+
+    @property
+    def requests_query(self) -> str:
+        query = r'''
+            select distinct ticker
+            from nasdaq.listed_stocks
+            where ticker !~ '[\^.~]'
+                and character_length(ticker) between 1 and 4
+            limit 10;
+            '''
         return query
 
-    def format_calls(self, idx, row) -> tuple:
-        company = row['ticker']
-        url_prefix = 'https://finance.yahoo.com/quote/'
-        url_suffix = '/cash-flow?p='
-        url = url_prefix + company + url_suffix + company
-        return url, company
+    def format_requests(self, row) -> tuple:
+        key = row.ticker
+        request = f'https://finance.yahoo.com/quote/{key}/cash-flow?p={key}'
+        return key, request
 
     def parse(self, soup, company) -> pd.DataFrame:
         dates = []
         tuples = []
         cnt_vals = 0
         table = soup.find('table')
-        for span in table.find_all('span'):
-            text = span.text
-            if re.match("^\d{2}\/\d{2}\/\d{4}$", text):
-                dates.append(text)
-            elif re.match("^[A-Za-z.^,/'\s_-]+$", text):
-                name = text
-                cnt_vals = 0
-            elif re.match("^(\d|-)?(\d|,)*\.?\d*$", text):
-                tuples.append((name, dates[cnt_vals], text))
-                cnt_vals = cnt_vals + 1
-        df = pd.DataFrame(tuples, columns=['metric', 'date', 'val'])
-        df['val'] = df['val'].str.replace(',', '').astype(float)
-        df['ticker'] = company
-        return df
 
-    def parallelize(self, url) -> pd.DataFrame:
-        soup = self.summon(url[0])
-        df = self.parse(soup, url.name)
-        time.sleep(self.len_of_pause)
+        if table:
+            for span in table.find_all('span'):
+                text = span.text
+                if re.match(r'^\d{2}\/\d{2}\/\d{4}$', text):
+                    dates.append(text)
+                elif re.match(r"^[A-Za-z.^,/'\s_-]+$", text):
+                    name = text
+                    cnt_vals = 0
+                elif re.match(r'^(\d|-)?(\d|,)*\.?\d*$', text):
+                    tuples.append((name, dates[cnt_vals], text))
+                    cnt_vals = cnt_vals + 1
+            df = pd.DataFrame(tuples, columns=['metric', 'date', 'val'])
+            df['val'] = df['val'].str.replace(',', '').astype(float)
+            df['ticker'] = company
+
+        else:
+            df = pd.DataFrame()
         return df
 
 
 if __name__ == '__main__':
-    CashFlowsScraper().execute()
+    CashFlow().execute()
