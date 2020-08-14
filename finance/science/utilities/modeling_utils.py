70445f9c789b434a169d5b6685fa3601671465c5
rbetzler <rbetzler@94.gmail.com>
2020-08-13 23:00:14 -0400

    tweaking same file function

--- finance/science/utilities/modeling_utils.py
+++ finance/science/utilities/modeling_utils.py
@@ -5,17 +5,16 @@ import pandas as pd
 
 def save_file(
         df: pd.DataFrame,
-        name: str,
+        subfolder: str,
+        filename: str = datetime.datetime.utcnow().strftime('%Y%m%d%H%M%S'),
         is_prod: bool = False,
         delimiter: str = ',',
 ):
     """Save dataframe to science folder"""
-    if is_prod:
-        subfolder = 'prod'
-    else:
-        subfolder = 'dev'
 
-    path = f"/usr/src/app/audit/science/{subfolder}/{name}/{datetime.datetime.utcnow().strftime('%Y%m%d%H%M%S')}.csv"
+    folder = 'prod' if is_prod else 'dev'
+
+    path = f'/usr/src/app/audit/science/{folder}/{subfolder}/{filename}.csv'
     df.to_csv(
         path,
         index=False,
