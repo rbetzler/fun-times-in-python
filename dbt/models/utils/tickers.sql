e8bb53f6f8fdbe739c679fddcf117d97d514e7cd
rbetzler <rbetzler@94.gmail.com>
2021-02-15 11:52:01 -0500

    Dedup dbt.tickers

--- dbt/models/utils/tickers.sql
+++ dbt/models/utils/tickers.sql
@@ -4,11 +4,21 @@
   )
 }}
 
-select distinct
-    ticker as symbol
-  , lower(regexp_replace(sector, '\W+', '_', 'g')) as sector
-  , lower(regexp_replace(industry, '\W+', '_', 'g')) as industry
-from {{ source('nasdaq', 'listed_stocks') }}
-where ticker !~ '[\^.~]'
-  and character_length(ticker) between 1 and 4
+with
+base as (
+  select
+      ticker as symbol
+    , lower(regexp_replace(sector, '\W+', '_', 'g')) as sector
+    , lower(regexp_replace(industry, '\W+', '_', 'g')) as industry
+    , row_number() over (partition by ticker order by file_datetime desc) as rn
+  from {{ source('nasdaq', 'listed_stocks') }}
+  where ticker !~ '[\^.~]'
+    and character_length(ticker) between 1 and 4
+)
+select
+    symbol
+  , sector
+  , industry
+from base
+where rn = 1
 order by 1
