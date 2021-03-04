5d92bb405b3ccfc2cb041e5e11d1cc49c917b119
rbetzler <rick.betzler@petalcard.com>
2021-03-03 20:51:30 -0500

    Accommodating mac in nasdaq scripts

--- scripts/nasdaq.sh
+++ scripts/nasdaq.sh
@@ -1,14 +1,28 @@
 
-cd /home/nautilus/Downloads
+if [[ $(uname -s) = "Darwin" ]]; then
+  cd /Users/rickbetzler/Downloads
+else
+  cd /home/nautilus/Downloads
+fi
 echo "In dir: $(pwd)"
 
-chromium-browser "https://www.nasdaq.com/market-activity/stocks/screener"
+url="https://www.nasdaq.com/market-activity/stocks/screener"
+if [[ $(uname -s) = "Darwin" ]]; then
+  open -a "Google Chrome" $url
+else
+  chromium-browser $url
+fi
 
 echo "Sleeping while the file downloads"
 sleep 20s
 
 file=$(ls -lt | grep "nasdaq" | awk '{print $9}' | head -1)
-target="/media/nautilus/fun-times-in-python/audit/nasdaq/listed_stocks/nasdaq_screener_$(date +"%Y%m%d%H%M%S").csv"
+if [[ $(uname -s) = "Darwin" ]]; then
+  prefix="/Users/rickbetzler/personal"
+else
+  prefix="/media/nautilus"
+fi
+target="$prefix/fun-times-in-python/audit/nasdaq/listed_stocks/nasdaq_screener_$(date +"%Y%m%d%H%M%S").csv"
 echo "Copying $file to $target"
 cp $file $target
 
@@ -16,15 +30,21 @@ echo "Running nasdaq loading, updating dbt tables, writing report"
 docker exec py-temp bash -c "python data/nasdaq/load.py; cd dbt; dbt run -m tickers; cd ..; python data/nasdaq/report.py"
 echo "Docker jobs complete"
 
-echo "Writing report to pi"
-PiIp="$(nmap -sn 192.168.1.0/24 | grep "pi" | awk -F"[()]" '{print $2}')"
-echo "Pi Ip identified as $PiIp"
+if [ $# -eq 0 ]; then
+  echo "Writing report to pi"
+  PiIp="$(nmap -sn 192.168.1.0/24 | grep "pi" | awk -F"[()]" '{print $2}')"
+  echo "Pi Ip identified as $PiIp"
 
-echo "Running pi startup script"
-ssh pi@$PiIp 'sh /home/pi/fun-times-in-python/scripts/pi.sh 0'
+  echo "Running pi startup script"
+  ssh pi@$PiIp 'sh /home/pi/fun-times-in-python/scripts/pi.sh 0'
 
-report="fun-times-in-python/audit/reports/listed_stocks/listed_stocks.csv"
-echo "Copying report to pi: $report"
-scp /media/nautilus/$report pi@$PiIp:/home/pi/$report
+  report="fun-times-in-python/audit/reports/listed_stocks/listed_stocks.csv"
+  echo "Copying report to pi: $report"
+  scp $prefix/$report pi@$PiIp:/home/pi/$report
+
+else
+  echo "Skipping pi sync"
+
+fi
 
 echo "Script finished"
